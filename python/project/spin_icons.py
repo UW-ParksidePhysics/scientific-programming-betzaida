ba522baa5a40704686fa2532483c9e14ed4d1a81
betzaidaal <alcai001@rangers.uwp.edu>
2025-05-01 07:08:56 -0500

    worked on planet spin icon

--- python/project/spin_icons.py
+++ python/project/spin_icons.py
@@ -15,233 +15,90 @@ __author__ = ["Betzaida Alcaide", "Maribel Moreno"]
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Global visualization settings
-FIG_SIZES = [(1.5, 1.5), (3, 3)]
-COLOR_MODES = [True, False]  # True = color, False = black & white
+# Extended planetary data including orbit and angular velocity
+planet_data = {
+    'mars': {'tilt': 25.2, 'a': 227.9e6, 'b': 227.9e6, 'omega': 360 / 24.6},
+    'earth': {'tilt': 23.4, 'a': 149.6e6, 'b': 149.6e6, 'omega': 360 / 24.0},
+    'pluto': {'tilt': 122.5, 'a': 5906.4e6, 'b': 5906.4e6, 'omega': 360 / 153.0},
+}
 
-# PLANETARY FUNCTIONS
-
-def label_icon(ax, planet_name, tilt_angle):
-    """
-    Author: Maribel Moreno
-    Adds label to the plot showing the planet name and tilt.
-    """
-    ax.set_title(f"{planet_name.capitalize()}\n{tilt_angle:.1f}°", fontsize=10)
-
-def draw_planet(ax, radius=1.0, color='gray'):
-    """
-    Author: Betzaida Alcaide
-    Draws a circle representing the planet.
-    """
-    circle = plt.Circle((0, 0), radius, color=color, ec='black', zorder=2)
-    ax.add_patch(circle)
-
-def draw_poles(ax, tilt_angle_deg, radius=1.0, color='black'):
-    """
-    Author: Betzaida Alcaide
-    Draws a line indicating the tilted pole axis.
-    """
-    angle_rad = np.radians(tilt_angle_deg)
-    x = radius * np.sin(angle_rad)
-    y = radius * np.cos(angle_rad)
-    ax.plot([0, x], [0, y], color=color, lw=2.0, zorder=3)
-
-def draw_orbit(ax, orbit_radius=2.5):
-    """
-    Author: Betzaida Alcaide
-    Draws a dashed orbit circle around the planet.
-    """
-    orbit = plt.Circle((0, 0), orbit_radius, fill=False, linestyle='--', edgecolor='gray', lw=0.8)
-    ax.add_patch(orbit)
-
-def draw_perpendicular_axis(ax, radius=1.5, color='black'):
-    """
-    Author: Betzaida Alcaide
-    Draws a vertical dashed line from which tilt is measured.
-    """
-    ax.plot([0, 0], [-radius, radius], linestyle='--', color=color, lw=1.0)
 
 def get_data():
-    """
-    Author: Betzaida Alcaide
-    This function returns the tilt angles of the planets.
-    """
-    planet_data = {
-        'mars': 25.2,
-        'earth': 23.44,
-        'pluto': 122.5
-    }
     return planet_data
 
-# TEST FUNCTION
 
-def test_plot():
-    """
-    Author: Maribel Moreno
-    Function to test basic visuals for a planet with tilt and orbit.
-    """
-    fig, ax = plt.subplots(figsize=(3, 3))
+def draw_planet_icon(planet_name, planet_info, color=True, size=(3, 3), save_path=None, time=0):
+    fig, ax = plt.subplots(figsize=size)
     ax.set_aspect('equal')
     ax.axis('off')
 
-    planet_name = 'earth'
-    tilt = 23.44
-    color = 'blue'
+    # Unpack parameters
+    tilt_angle = planet_info['tilt'] + planet_info['omega'] * time
+    angle_rad = np.radians(tilt_angle)
+    radius = 1.0
+    orbit_radius = 2.5
 
-    draw_orbit(ax)
-    draw_planet(ax, color=color)
-    draw_perpendicular_axis(ax)
-    draw_poles(ax, tilt, color='orange')
-    label_icon(ax, planet_name, tilt)
+    # Colors
+    planet_color = 'gray' if not color else {'earth': 'blue', 'mars': 'red', 'pluto': 'brown'}.get(planet_name, 'gray')
 
-    plt.show()
+    # Draw orbit ellipse
+    orbit = plt.Circle((0, 0), orbit_radius, fill=False, color='gray', linestyle='--', lw=0.5)
+    ax.add_patch(orbit)
 
-# MAIN EXECUTION BLOCK
+    # Draw planet
+    circle = plt.Circle((0, 0), radius, color=planet_color, ec='black', zorder=2)
+    ax.add_patch(circle)
+
+    # Equator
+    ax.plot([-radius, radius], [0, 0], color='black', lw=1.2, zorder=3)
+
+    # Perpendicular axis (from orbit)
+    ax.plot([0, 0], [-1.5 * radius, 1.5 * radius], color='black', lw=1.0, linestyle='--', zorder=1)
+
+    # Tilt axis
+    x_tilt = radius * np.sin(angle_rad)
+    y_tilt = radius * np.cos(angle_rad)
+    ax.plot([0, x_tilt], [0, y_tilt], color='yellow' if color else 'black', lw=2.0, zorder=4)
+
+    # Arc for tilt angle
+    arc_radius = 0.5
+    arc_angles = np.linspace(0, angle_rad, 50)
+    arc_x = arc_radius * np.sin(arc_angles)
+    arc_y = arc_radius * np.cos(arc_angles)
+    ax.plot(arc_x, arc_y, color='black', lw=1.0)
+
+    # Angle label
+    label_x = arc_radius * np.sin(angle_rad / 2)
+    label_y = arc_radius * np.cos(angle_rad / 2)
+    ax.text(label_x, label_y, f"{tilt_angle:.1f}°", fontsize=8, ha='left', va='bottom')
+
+    # Rotation arrow along equator (semicircular above planet)
+    theta = np.linspace(-np.pi / 2, np.pi / 2, 100)
+    r = radius
+    arrow_x = r * np.cos(theta)
+    arrow_y = 0.15 * radius * np.sin(theta)  # slight vertical offset to appear above the equator
+    ax.plot(arrow_x, arrow_y, color='black', lw=1.0)
+    ax.annotate('', xy=(arrow_x[-1], arrow_y[-1]), xytext=(arrow_x[-2], arrow_y[-2]),
+                arrowprops=dict(arrowstyle="->", lw=1.0))
+
+    # Label
+    ax.set_title(planet_name.capitalize(), fontsize=10)
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches='tight', dpi=300)
+    plt.close()
+
+
+def run_all():
+    data = get_data()
+    for planet, info in data.items():
+        for size in [(3, 3), (1.5, 1.5)]:
+            draw_planet_icon(planet, info, color=True, size=size, save_path=f"{planet}_color_{size[0]}x{size[1]}.png")
+            draw_planet_icon(planet, info, color=False, size=size, save_path=f"{planet}_bw_{size[0]}x{size[1]}.png")
+
 
 if __name__ == '__main__':
-    data = get_data()
-    for planet, tilt in data.items():
-        print(f"{planet} = {tilt}°")
-    test_plot()
-
-
-
-# def label_icon(ax, tilt_deg, planet_name):
-#    """
-#    Author: Maribel moreno
-#    This will label the icon with the planet's name and its tilt angle
-#    """
-    #Formats the label text
-#    label = f"{planet_name.title()}: {tilt_deg:.1f}°"
-
-    #Places the label
-#    ax.text(0.5, 1.4, label,
-#            ha='center', va='center',
-#            fontsize=10, fontweight='bold',)
-
-
-# def draw_planet():
-#    """
-#    Author: Maribel Moreno
-#    This draws the orbit, perpendicular axis, axial tilt, and label for each planet defined in gen_data()
-#    """
-#    data = get_data()
-#
-#    for planet, tilt in data.items():
-#        fig, ax = plt.subplots(figsize=(3, 3))
-
-        # Draw components
-#        draw_orbit(ax)
-#        draw_perpendicular_axis(ax)
-#        draw_poles(ax, tilt)
-#        label_icon(ax, tilt, planet)
-
-        # Plot styling
-#        ax.set_aspect('equal')
-#        ax.set_xlim(-2, 2)
-#        ax.set_ylim(-2, 2)
-#        ax.axis('off')     # removes axes for clear icon
-#        plt.title(f"{planet.title()} Spin Icon", fontsize=12)
-
-        # Show or save
-#        plt.show()
-
-
-# def draw_poles(ax, tilt_deg, length=1.5):
-#    """
-#    Author: Maribel Moreno
-#    This will draw the axial tilt of the planet from the center
-#    """
-    # Converts angle to radians
-#    tilt_rad = np.radians(tilt_deg)
-
-    # Computes line endpoints using trig
-#    x = np.array([-length * np.sin(tilt_rad), length * np.sin(tilt_rad)])
-#    y = np.array([-length * np.cos(tilt_rad), length * np.cos(tilt_rad)])
-
-    #Plots the tilted axis
-#    ax.plot(x, y, 'r--', linewidth=2, label=f'Tilt = {tilt_deg:.1f}°')
-
-
-# def draw_orbit(ax, radius=1.0):
-#    """
-#    Author: Betzaida Alcaide
-#    This will draw the planets orbital plane of the planet
-#    """
-    # Creates angle values from 0 to 2*pi
-#    theta = np.linspace(0, 2 * np.pi, 100)
-
-    # Parametric equations for a circle (x = r*cos, y = r*sin)
-#    x = radius * np.cos(theta)
-#    y = radius * np.sin(theta)
-
-    #Plots the orbit line
-#    ax.plot(x, y, 'k', label='Orbital Plane')
-
-
-# def draw_perpendicular_axis(ax, length=1.5):
-#    """
-#    Author: Betzaida Alcaide
-#     This will draw a perpendicular axis of the planet from the center perpendicular to the orbit
-#    """
-    #The line that goes from -length to +length vertically
-#    x = [0, 0]
-#    y = [-length, length]
-
-#    ax.plot(x, y, 'b', linewidth=2, label='Perpendicular Axis')
-
-# def get_data():
-#    """
-#    Author: Betzaida Alcaide
-#    This will show the tilt angle of the 3 planets
-#    """
-#    planet_data = {
-#        'mars': 25.2,
-#        'earth': 23.44,
-#        'pluto': 122.5
-#    }
-
-#    return planet_data
-
-
-
-# def test_plot():
-
-    #line
-#    intercepts = [4, 3]
-#    x_bounds = np.array([-3, 8])
-#    x_values = np.linspace(x_bounds[0], x_bounds[1], 100)
-#    #y_bounds = -intercepts[1] * x_bounds / intercepts[0] + intercepts[1]
-#    y_values = -intercepts[1] * x_values / intercepts[0] + intercepts[1]
-
-    #circle
-#    radius = intercepts[0]
-#    angles = np.linspace( 0, 2 * np.pi, 100)
-#    circle_xs = radius * np.cos(angles)
-#    circle_ys = radius * np.sin(angles)
-#    #plt.plot(x_bounds, y_bounds)
-#    plt.plot(x_values, y_values)
-#    plt.plot(circle_xs, circle_ys)
-#    plt.show()
-
-#    return
-
-# if __name__ == '__main__':
-#   draw_planet()
-
-
-    # data = get_data()
-   # for planet, tilt in data.items():
-   #     print(f"{planet} = {tilt}°")
-   # test_plot()
-
-
-
-
-
-
-
-
+    run_all()
 
 
 
